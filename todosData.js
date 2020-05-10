c3b368c59b349f892b2a703c16409c0802a062c6
Cody-Wales <56929727+Cody-Wales@users.noreply.github.com>
2020-05-10 17:53:46 -0600

    Add files via upload

---
+++ todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Finish math homework",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Wash dishes",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Wash clothes",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Eat dinner",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on JoJo",
+        completed: false
+    }
+]
+
+export default todosData
